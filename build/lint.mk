9926b6bb4e60a303db9eea4288cdcab2573cd179
Zach Leslie <xaque208@gmail.com>
2026-02-22 04:05:29 +0000

    Run lint with the tools image

--- build/lint.mk
+++ build/lint.mk
@@ -1,56 +1,53 @@
 #
 # Makefile fragment for Linting
 #
+# Linters run in the tools container via RUN_TOOL (see build/tools.mk).
+# No tools are installed on the host.
+#
 
 GO           ?= go
 MISSPELL     ?= misspell
 GOFMT        ?= gofmt
 GOIMPORTS    ?= goimports
-
-GOLINTER      = golangci-lint
-
-EXCLUDEDIR      ?= .git
-SRCDIR          ?= .
-GO_PKGS         ?= $(shell ${GO} list ./... | grep -v -e "/vendor/" -e "/example")
-FILES           ?= $(shell find ${SRCDIR} -type f | grep -v -e '.git/' -e '/vendor/')
-GO_FILES        ?= $(shell find $(SRCDIR) -type f -name "*.go" | grep -v -e ".git/" -e '/vendor/' -e '/example/')
-PROJECT_MODULE  ?= $(shell $(GO) list -m)
-
+GOLINTER     ?= golangci-lint
 GO_MOD_OUTDATED ?= go-mod-outdated
 
+EXCLUDEDIR   ?= .git
+SRCDIR       ?= .
+GO_PKGS      ?= $(shell $(GO) list ./... | grep -v -e "/vendor/" -e "/example")
+FILES        ?= $(shell find $(SRCDIR) -type f | grep -v -e '.git/' -e '/vendor/')
+GO_FILES     ?= $(shell find $(SRCDIR) -type f -name "*.go" | grep -v -e ".git/" -e '/vendor/' -e '/example/')
+PROJECT_MODULE ?= $(shell $(GO) list -m 2>/dev/null)
 
 lint: deps spell-check gofmt lint-commit golangci goimports outdated
 lint-fix: deps spell-check-fix gofmt-fix goimports
 
-#
-# Check spelling on all the files, not just source code
-#
 spell-check: deps
 	@echo "=== $(PROJECT_NAME) === [ spell-check      ]: Checking for spelling mistakes with $(MISSPELL)..."
-	@$(MISSPELL) -source text $(FILES)
+	@$(RUN_TOOL) $(MISSPELL) -source text $(FILES)
 
 spell-check-fix: deps
 	@echo "=== $(PROJECT_NAME) === [ spell-check-fix  ]: Fixing spelling mistakes with $(MISSPELL)..."
-	@$(MISSPELL) -source text -w $(FILES)
+	@$(RUN_TOOL) $(MISSPELL) -source text -w $(FILES)
 
 gofmt: deps
 	@echo "=== $(PROJECT_NAME) === [ gofmt            ]: Checking file format with $(GOFMT)..."
-	@gofmt -e -l -s -d $(GO_FILES)
+	@$(RUN_TOOL) gofmt -e -l -s -d $(GO_FILES)
 
 gofmt-fix: deps
 	@echo "=== $(PROJECT_NAME) === [ gofmt-fix        ]: Fixing file format with $(GOFMT)..."
-	@gofmt -e -l -s -w $(GO_FILES)
+	@$(RUN_TOOL) gofmt -e -l -s -w $(GO_FILES)
 
 goimports: deps
 	@echo "=== $(PROJECT_NAME) === [ goimports        ]: Checking imports with $(GOIMPORTS)..."
-	@$(GOIMPORTS) -l -w -local $(PROJECT_MODULE) $(GO_FILES)
+	@$(RUN_TOOL) $(GOIMPORTS) -l -w -local $(PROJECT_MODULE) $(GO_FILES)
 
 golangci: deps
 	@echo "=== $(PROJECT_NAME) === [ golangci-lint    ]: Linting using $(GOLINTER)"
-	@$(GOLINTER) run
+	@$(RUN_TOOL) $(GOLINTER) run
 
 outdated: deps
 	@echo "=== $(PROJECT_NAME) === [ outdated         ]: Finding outdated deps with $(GO_MOD_OUTDATED)..."
-	@$(GO) list -u -m -json all | $(GO_MOD_OUTDATED) -direct -update
+	@$(RUN_TOOL) sh -c '$(GO) list -u -m -json all | $(GO_MOD_OUTDATED) -direct -update'
 
-.PHONY: lint spell-check spell-check-fix gofmt gofmt-fix lint-fix lint-commit outdated goimports
+.PHONY: lint spell-check spell-check-fix gofmt gofmt-fix lint-fix lint-commit outdated goimports golangci
